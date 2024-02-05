d8129988a71846eb65fb15d59b7ca35fd8e1139c
dhjs0000 <3110197220@qq.com>
2024-02-05 19:10:16 +0800

    upj

---
+++ com.bbyc/src/com/bbyc/gui/gui.java
@@ -0,0 +1,56 @@
+package com.bbyc.gui;
+
+import javax.swing.*;
+
+public class gui {
+
+    private JTree tree1;
+    private JList list1;
+    private JTabbedPane tabbedPane1;
+    private JSlider slider1;
+    private JEditorPane editorPane1;
+    private JTextPane textPane1;
+    private JTextArea textArea1;
+    private JTextField textField1;
+    private JRadioButton radioButton1;
+    private JCheckBox checkBox1;
+    private JFormattedTextField formattedTextField1;
+    private JScrollBar scrollBar1;
+    private JProgressBar progressBar1;
+    private JTabbedPane tabbedPane2;
+    private JButton button1;
+    private JRadioButton radioButton2;
+    private JCheckBox checkBox2;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JFormattedTextField formattedTextField2;
+    private JTextArea textArea2;
+    private JPasswordField passwordField2;
+    private JFormattedTextField formattedTextField3;
+    private JTextArea textArea3;
+    private JTextPane textPane2;
+    private JEditorPane editorPane2;
+    private JComboBox comboBox1;
+    private JTable table1;
+    private JList list2;
+    private JTree tree2;
+    private JTabbedPane tabbedPane3;
+    private JSpinner spinner1;
+    private JSlider slider2;
+    private JProgressBar progressBar2;
+    private JScrollBar scrollBar2;
+    private JButton button2;
+    private JRadioButton radioButton3;
+    private JCheckBox checkBox3;
+    private JTextField textField3;
+    private JPasswordField passwordField3;
+    private JFormattedTextField formattedTextField4;
+    private JTextArea textArea4;
+    private JTextPane textPane3;
+    private JEditorPane editorPane3;
+    private JComboBox comboBox2;
+    private JTable table2;
+    private JList list3;
+    private JTree tree3;
+    private JTabbedPane tabbedPane4;
+}
---
+++ com.bbyc/src/com/bbyc/gui/guitest.java
@@ -0,0 +1,65 @@
+package com.bbyc.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class guitest extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+
+    public guitest() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // 点击 X 时调用 onCancel()
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // 遇到 ESCAPE 时调用 onCancel()
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        System.out.println("OK");
+        dispose();
+    }
+
+    private void onCancel() {
+        // 必要时在此处添加您的代码
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        guitest dialog = new guitest();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
